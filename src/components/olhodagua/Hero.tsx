65cad5925f4919d35e219652660d240414189312
flaviousilva <flavioulisses2@gmail.com>
2025-04-28 17:54:14 -0300

    Update Hero.tsx

--- src/components/olhodagua/Hero.tsx
+++ src/components/olhodagua/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => (
     className="relative h-[80vh] min-h-[480px] flex items-center bg-soft-blue overflow-hidden"
   >
     <img
-      src="https://raw.githubusercontent.com/flaviousilva/imagens/b0b27a1ee8198d8c1e7f56fede8faae7e6420c56/DJI_0009.JPG"
+      src="https://raw.githubusercontent.com/flaviousilva/imagens/da095067c0bce0d3a81a2a3a39d702dc97a48048/Faz%20Olho%20Dagua%20DJI_0060.JPG"
       alt="Fazenda de camarão"
       className="absolute inset-0 w-full h-full object-cover object-center opacity-70"
       draggable="false"
