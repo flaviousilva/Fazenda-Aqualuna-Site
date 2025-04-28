e82f721dcd420aa67808856029155fa2dfa70a72
flaviousilva <flavioulisses2@gmail.com>
2025-04-28 14:10:15 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 const Footer = () => (
   <footer className="bg-aqua text-white py-6 mt-4">
     <div className="container text-center flex flex-col gap-1">
-      <span className="font-playfair text-lg font-bold">Fazenda Feliz 🦐</span>
+      <span className="font-playfair text-lg font-bold">Fazenda Aqualuna 🦐</span>
       <span className="font-inter text-sm">
         © {new Date().getFullYear()} Todos os direitos reservados.
       </span>
