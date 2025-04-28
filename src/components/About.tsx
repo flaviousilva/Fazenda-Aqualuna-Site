219d1f3010e58a761bed5658c46d239f453f326c
flaviousilva <flavioulisses2@gmail.com>
2025-04-28 14:43:39 -0300

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,7 +13,7 @@ const About = () => (
       <h2 className="font-playfair text-3xl font-bold text-aqua mb-4">Sobre nós</h2>
       <p className="font-inter text-gray-700 text-lg mb-4">
         Somos referência em criação sustentável de camarão no Brasil.
-        Há mais de 24 anos, produzimos camarão de altíssima qualidade respeitando o meio ambiente,
+        Há mais de 25 anos, produzimos camarão de altíssima qualidade respeitando o meio ambiente,
         garantindo sabor, segurança alimentar e responsabilidade social.
       </p>
       <ul className="font-inter text-md text-gray-700 mt-2 space-y-2 list-disc list-inside">
