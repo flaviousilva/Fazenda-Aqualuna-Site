e47b380bc79cd5621dcad5a94933b9d274224235
flaviousilva <flavioulisses2@gmail.com>
2025-04-29 13:43:34 -0300

    Update Navbar.tsx

--- src/components/aqualuna/Navbar.tsx
+++ src/components/aqualuna/Navbar.tsx
@@ -36,7 +36,7 @@ const Navbar = () => {
             <img 
               src="/lovable-uploads/3d33c833-f0e1-4e81-87e8-818dfc317faf.png" 
               alt="Olho D'Água Logo" 
-              className="h-8 w-auto" 
+              className="h-12 w-auto" 
             /> 
             Olho D'Água
           </Link>
