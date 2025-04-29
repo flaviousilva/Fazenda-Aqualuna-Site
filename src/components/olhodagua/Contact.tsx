d0f529ba628e4d677ffda8f2ca4f459ce8f0590b
flaviousilva <flavioulisses2@gmail.com>
2025-04-29 13:38:32 -0300

    Update Contact.tsx

--- src/components/olhodagua/Contact.tsx
+++ src/components/olhodagua/Contact.tsx
@@ -58,7 +58,7 @@ const Contact = () => {
         </form>
         <div className="mt-8 text-center text-gray-700 font-inter">
           <strong>Localização:</strong> Rua Engenho Amparo , ILHA DE ITAMARACÁ - PE, 53.900-000, Brasil <br />
-          <strong:</strong>{" "}
+          <strong>Telefone : </strong>{" "}
           <a href="https://wa.me/559999999999" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline hover:text-blue-700">
 
           </a>
